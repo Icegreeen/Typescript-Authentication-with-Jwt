e49ece5ac4e806460cab65a968fbc9ac752d61ca
Icegreeen <flavioaquila16@gmail.com>
2023-09-12 18:59:06 -0300

    added authorization

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { ApiError, BadRequestError, NotFoundError, UnauthorizedError} from './helpers/api-erros'
 import { UserControlller } from './controllers/UserController';
+import { User } from './entities/User';
 
 const routes = Router();
 
 routes.post('/user', new UserControlller().create)
 routes.post('/login', new UserControlller().login)
+
+routes.get('/profile', new UserControlller().getProfile)
 
 export default routes;
